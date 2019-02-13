4d712b650025e33c546b7a8b905ed67b2c21aa01
Sergey korablev <adamas34@gmail.com>
2019-02-13 03:10:26 +0300

    hamburger menu

--- docs/hamburger.js
+++ docs/hamburger.js
@@ -1,8 +1,10 @@
 const hamburgerIcon = document.querySelector('.hamburger-menu-link');
-const hamburgerMenu= document.querySelector('.hamburger-menu');
-const hamburgerMenuItem = document.querySelectorAll('#nav__item');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerMenuItems = document.querySelectorAll('#nav__item');
 const hamburgerCloseIcon = document.querySelector('.close');
 const hamburgerLogo = document.querySelector('#logo__link');
+
+
 
 hamburgerIcon.addEventListener('click', function (event) {
     event.preventDefault();
@@ -22,3 +24,21 @@ hamburgerLogo.addEventListener('click', function (event) {
     hamburgerIcon.style.display = '';
     document.body.style.overflow = '';
 })
+
+for (var hamburgerMenuItem of hamburgerMenuItems) {
+
+    hamburgerMenuItem.addEventListener('click', function (event) {
+        var curHamburgerMenuItem = event.currentTarget;
+       
+       var contentHamburgerMenuItem = curHamburgerMenuItem.querySelector('.nav__link');
+       
+       var hrefHamburgerMenuItem = contentHamburgerMenuItem.getAttribute('href');
+       
+        event.preventDefault();
+        hamburgerMenu.style.display = '';
+        hamburgerIcon.style.display = '';
+        document.body.style.overflow = '';
+        
+        window.location.hash = hrefHamburgerMenuItem;
+    })
+}
